8334c9acfc1dae2f39766efd7caff3875854145b
NathanSnail <nathan.titirangi@gmail.com>
2024-04-23 23:16:40 +1200

    RegisterGunAction

--- main.py
+++ main.py
@@ -158,6 +158,9 @@ overrides = {
 		"comment": "Deprectated, use EntityAddComponent2 instead.",
 		"deprecated": True,
 	},
+	"RegisterGunAction": {
+		"args": "action_id:string, action_name:string, action_description:string, action_sprite_filename:string, action_unidentified_sprite_filename:string, action_type:number, action_spawn_level:string, action_spawn_probability:string, action_spawn_requires_flag:nil, action_spawn_manual_unlock:boolean, action_max_uses:number, custom_xml_file:string, action_mana_drain:number, action_is_dangerous_blast:nil, action_draw_many_count:number, action_ai_never_uses:boolean, action_never_unlimited:boolean, state_shuffled:boolean, state_cards_drawn:number, state_discarded_action:boolean, state_destroyed_action:boolean, fire_rate_wait:number, speed_multiplier:number, child_speed_multiplier:number, dampening:number, explosion_radius:number, spread_degrees:number, pattern_degrees:number, screenshake:number, recoil:number, damage_melee_add:number, damage_projectile_add:number, damage_electricity_add:number, damage_fire_add:number, damage_explosion_add:number, damage_ice_add:number, damage_slice_add:number, damage_healing_add:number, damage_curse_add:number, damage_drill_add:number, damage_null_all:number, damage_critical_chance:number, damage_critical_multiplier:number, explosion_damage_to_materials:number, knockback_force:number, reload_time:number, lightning_count:number, material:string, material_amount:number, trail_material:string, trail_material_amount:number, bounces:number, gravity:number, light:number, blood_count_multiplier:number, gore_particles:number, ragdoll_fx:number, friendly_fire:boolean, physics_impulse_coeff:number, lifetime_add:number, sprite:string, extra_entities:string, game_effect_entities:string, sound_loop_tag:nil, projectile_file:string "
+	},
 }
 
 
